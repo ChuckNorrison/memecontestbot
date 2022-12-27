39ff9e8fa03abbe92bbbe3a2bd67cca3ee669ef7
ChuckNorrison <2964146+ChuckNorrison@users.noreply.github.com>
2022-12-27 21:39:39 +0100

    use config file and argument
    use a config file
    
    config.py as default config file or override with argument -c or --config

--- memecontestbot.py
+++ memecontestbot.py
@@ -10,58 +10,44 @@ Start bot to create a nice ranking.
 
 from pyrogram import Client, enums
 from datetime import datetime
+from os import path
+from argparse import ArgumentParser
 import csv
 import re
 
 app = Client("my_account")
 
-#########################
-# START TWEAK CONFIG HERE
+# import config
+parser = ArgumentParser()
+parser.add_argument("-c", "--config", dest="configfile",
+                    help="path to file with your config", metavar="FILE")
+args = parser.parse_args()
 
-# channel name for public channels 
-# or chat id for private chats to analyze
-chat_id = "mychannelname" 
+if args.configfile:
+    configfile = args.configfile
+else:
+    configfile = "config.py"
 
-# only posts prior this date and time will get analyzed
-contest_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
-#contest_date = "2022-12-19 23:59:59" # example with fixed date and time
-
-# only posts newer than x days will be ranked. 
-# 1 = 24h contest without duplicates, 
-# 2+ days post with same author gets added
-contest_days = 1  
-
-# amount of winners to honor in ranking message
-contest_max_ranks = 10 
-
-# posts we want to exclude from ranking. 
-# Add your patterns to this array.
-exclude_pattern = ["Meme Contest", "Tagessieger", "Rangliste"] 
-
-# simple text footer in ranking view, 
-# should be used to identify exclude posts
-final_message_footer = f"🏆 [{exclude_pattern[0]}](https://t.me/mychannelname) 🏆"
-
-# Send the final message to a given chat id 
-# with ranking and winner photo or set to False
-final_message_chat_id = False 
-
-# link the ranked post 
-# in final message on the result counter
-post_link = True 
-
-# Create a CSV file with all participants found
-create_csv = True
-
-# Send CSV file to a given chat id
-csv_chat_id = final_message_chat_id
-
-# Add winner photo in final message
-# True to add or False to disable the winner photo for final message
-post_winner_photo = False
-
-# END TWEAK CONFIG
-#########################
+if path.exists(configfile): 
+    exec(compile(open(configfile).read(), configfile, 'exec'))
+    try:
+        chat_id
+        contest_date
+        contest_days
+        contest_max_ranks
+        exclude_pattern
+        final_message_footer
+        final_message_chat_id
+        post_link
+        create_csv
+        csv_chat_id
+        post_winner_photo
+    except:
+        print(f"Can not read from config file '{configfile}', please checkout config.py")
+        quit()
+else:
+    print(f"No config file found as '{configfile}', please checkout config.py as reference")
+    quit()
 
 # global vars
 participants = []
